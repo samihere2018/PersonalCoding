1979bce0151a18fb15f77e9312c7229721e0ce43
Sylvia Amihere <samihere@crimson.ua.edu>
2025-06-24 12:12:24 -0500

    matrix-system

---
+++ matrix_system.py
@@ -0,0 +1,14 @@
+#---------------------------------------------------------------------------------------------------------
+#Author(s): Sylvia Amihere @ SMU
+#Description: The example below solves a real-valued and complex valued matrix-system for the solution x.
+#---------------------------------------------------------------------------------------------------------
+
+# --------------------------------------------------------------------------------------------------------
+# This example solves the real-valued and complex-valued matrix system
+# Real-valued:    A = 
+#                 b = 
+# Complex-valued: A = 
+#                 b = 
+#  -------------------------------------------------------------------------------------------------------
+
+import numpy as np
